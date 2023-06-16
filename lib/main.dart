5bba7d468f3714fbde6ed51ccf3c062f5a9713f7
Anand-Jangid <jangidme88@gmail.com>
2023-06-16 20:33:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:infinite_list_bloc/simple_bloc_observer.dart';
+
+import 'app.dart';
+
+void main() {
+  // Bloc.observer = const SimpleBlocObserver();
+  runApp(const App());
+}
+
---
+++ lib/posts/posts.dart
@@ -0,0 +1,4 @@
+export 'bloc/post_bloc.dart';
+export 'models/posts.dart';
+export 'view/view.dart';
+export 'widgets/widgets.dart';
